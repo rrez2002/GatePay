917aeefde9517997711012a29686aef3f0285c1c
reza rezaei <rrez2002@gmail.com>
2023-09-09 22:34:41 +0330

    add payment class

---
+++ src/payment.ts
@@ -0,0 +1,159 @@
+import Invoice from "./invoice";
+import { Driver } from "./abstracts/driver";
+import { Setting } from "./contracts/interface/setting.interface";
+import { driverApis, drivers } from "./config/payment";
+import { Gateway } from "./gateway";
+
+type driverType = keyof typeof drivers;
+
+export class Payment {
+  public settings: Setting;
+  public callbackUrl: string;
+  public driver: string;
+  public driverInstance: Driver;
+  public invoice: Invoice;
+
+  constructor(driver: string) {
+    this.invoice = new Invoice();
+    this.setDriver(driver);
+  }
+
+  /**
+   *
+   * @param url
+   * @returns Payment
+   */
+  setCallbackUrl(url: string) {
+    this.callbackUrl = url;
+
+    return this;
+  }
+
+  /**
+   *
+   * @returns Payment
+   */
+  resetCallbackUrl() {
+    this.setCallbackUrl(driverApis[this.driver].callbackUrl);
+
+    return this;
+  }
+
+  /**
+   *
+   * @param amount
+   * @returns Payment
+   */
+  setAmount(amount: number) {
+    this.invoice.setAmount(amount);
+
+    return this;
+  }
+
+  /**
+   *
+   * @param id
+   * @returns Payment
+   */
+  setTransactionId(id: string) {
+    this.invoice.setTransactionId(id);
+
+    return this;
+  }
+
+  /**
+   *
+   * @param driver
+   * @returns Payment
+   */
+  setDriver(driver: string) {
+    this.validateDriver(driver);
+    this.driver = driver;
+    this.invoice.setDriver(driver);
+    this.setGatewayApi();
+    this.setDriverInstance(driver);
+
+    return this;
+  }
+
+  /**
+   *
+   * @param driver
+   * @returns Payment
+   */
+  setDriverInstance(driver: string) {
+    this.driverInstance = new drivers[driver as driverType](
+      this.invoice,
+      this.settings,
+    );
+    return this;
+  }
+
+  /**
+   *
+   * @returns Driver
+   */
+  getDriverInstance() {
+    if (this.driverInstance) return this.driverInstance;
+    return this.setDriverInstance(this.driver);
+  }
+
+  /**
+   *
+   * @param driver
+   * @returns boolean
+   */
+  validateDriver(driver: string) {
+    if (driver in drivers) {
+      return true;
+    }
+    throw new Error("driver is invalid");
+  }
+
+  /**
+   *
+   * @returns Payment
+   */
+  setGatewayApi() {
+    this.settings = driverApis[this.driver];
+
+    return this;
+  }
+
+  /**
+   *
+   * @param invoice
+   * @param callbackUrl
+   * @returns Payment
+   */
+  async purchase(invoice?: Invoice, callbackUrl?: string) {
+    if (invoice) this.invoice = invoice;
+    if (callbackUrl) this.setCallbackUrl(callbackUrl);
+
+    const transactionId = await this.driverInstance.purchase();
+
+    this.invoice.setTransactionId(transactionId);
+
+    return this;
+  }
+
+  /**
+   *
+   * @returns Payment
+   */
+  pay(): Gateway {
+    this.getDriverInstance();
+
+    return this.driverInstance.pay();
+  }
+
+  /**
+   *
+   * @returns Payment
+   */
+  verify() {
+    this.getDriverInstance();
+
+    return this.driverInstance.verify();
+  }
+}

d75e43a44d3bd0e6918de0853a1baafd801bc9da
reza rezaei <rrez2002@gmail.com>
2023-10-21 19:23:47 +0330

    export idpay driver on drivers

--- src/drivers/index.ts
+++ src/drivers/index.ts
@@ -1,3 +1,4 @@
 export { PayIR } from "./payir/payir.driver";
 export { Zarinpal } from "./zarinpal/zarinpal.driver";
 export { Zibal } from "./zibal/zibal.driver";
+export { Idpay } from "./idpay/idpay.driver";

67fa67935ebdf45718aa425657db72a052b855af
reza rezaei <rrez2002@gmail.com>
2023-10-27 11:48:17 +0330

    fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,9 +4,9 @@ const config: Config.InitialOptions = {
   verbose: true,
   collectCoverage: true,
   coverageProvider: "v8",
-  transform: {
-    "^./test/+\\.test.ts?$": "ts-jest",
-  },
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testRegex: "test/.*.test.ts$",
   setupFiles: ["dotenv/config"],
 };
 export default config;

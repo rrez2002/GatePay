0ac70658a1c54379de981e63442e38829f693bdd
reza rezaei <rrez2002@gmail.com>
2023-11-20 20:53:47 +0330

    fix verify response type

--- src/abstracts/driver.ts
+++ src/abstracts/driver.ts
@@ -2,6 +2,7 @@ import { Gateway } from "../gateway";
 import { Setting } from "../contracts/interface";
 import Invoice from "../invoice";
 import axios from "axios";
+import { Receipt } from "./receipt";
 
 export abstract class Driver<T extends Invoice<any>> {
   protected invoice: T;
@@ -30,5 +31,5 @@ export abstract class Driver<T extends Invoice<any>> {
   abstract purchase(): Promise<string>;
 
   abstract pay(): Gateway;
-  abstract verify(): Promise<any>;
+  abstract verify(): Promise<Receipt<any>>;
 }

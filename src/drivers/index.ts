6c74445c3769f17b6bbbeb80141895af77e200d4
reza rezaei <rrez2002@gmail.com>
2023-12-25 18:58:17 +0330

    fix: add sep driver to drivers

--- src/drivers/index.ts
+++ src/drivers/index.ts
@@ -5,3 +5,4 @@ export { Zibal } from "./zibal/zibal.driver";
 export { Idpay } from "./idpay/idpay.driver";
 export { Payping } from "./payping/payping.driver";
 export { Atipay } from "./atipay/atipay.driver";
+export { Sep } from "./sep/sep.driver";

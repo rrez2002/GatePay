4da9b9f27d001ce5aa8151e91d7b70dcc932e8f4
reza rezaei <rrez2002@gmail.com>
2023-12-25 18:57:49 +0330

    fix: add atapay to drivers

--- src/drivers/index.ts
+++ src/drivers/index.ts
@@ -4,3 +4,4 @@ export { Zarinpal } from "./zarinpal/zarinpal.driver";
 export { Zibal } from "./zibal/zibal.driver";
 export { Idpay } from "./idpay/idpay.driver";
 export { Payping } from "./payping/payping.driver";
+export { Atipay } from "./atipay/atipay.driver";

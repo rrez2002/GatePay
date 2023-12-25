95b9f0ac52c191150eaa36b54dbc06c964f0c712
reza rezaei <rrez2002@gmail.com>
2023-12-25 21:59:01 +0330

    feat: render payment form

--- src/gateway.ts
+++ src/gateway.ts
@@ -1,8 +1,10 @@
+import { join } from "path";
 import {
   GatewayInterface,
   InputType,
   ToJsonResponse,
 } from "./contracts/interface";
+import { readFileSync } from "fs";
 
 export class Gateway implements GatewayInterface {
   constructor(
@@ -19,6 +21,28 @@ export class Gateway implements GatewayInterface {
     };
   }
   toHtml() {
-    //TODO: generate html page
+    let filePath = join(`${__dirname}/assets/redirect_form.asset.html`);
+
+    let inputs = "";
+    let file = readFileSync(filePath, "utf8").toString();
+    let inputFilePath = join(`${__dirname}/assets/input.asset.html`);
+    let inputFile: string;
+
+    file = file
+      .replace("{{ACTION}}", this.url)
+      .replace("{{METHOD}}", this.method);
+    this.inputs.forEach((input: InputType) => {
+      inputFile = readFileSync(inputFilePath, "utf8").toString();
+
+      inputFile = inputFile
+        .replace("{{NAME}}", input.key)
+        .replace("{{VALUE}}", input.value);
+
+      inputs += `${inputFile}\b\t`;
+    });
+
+    file = file.replace("{{INPUT}}", inputs);
+
+    return file;
   }
 }
